db0a100fd295abae672e49a6810cf06c42ef289b
tjdgusdk <sha6639@naver.com>
2024-09-23 10:49:54 +0900

    막대기

---
+++ src/Main_17608.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main_17608 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] heights = new int[N];
+
+        for (int i=0; i<N; i++) {
+            heights[i] = sc.nextInt();
+        }
+
+        int visibleCount = 0;
+        int maxHeight = 0;
+
+        for (int i=N-1; i>=0; i--) {
+            if (heights[i] > maxHeight) {
+                visibleCount++;
+                maxHeight = heights[i];
+            }
+        }
+
+        System.out.println(visibleCount);
+        sc.close();
+    }
+}
