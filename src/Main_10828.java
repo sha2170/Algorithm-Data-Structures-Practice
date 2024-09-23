2ff5651d2a5cea84f16969621528e061c65a507c
tjdgusdk <sha6639@naver.com>
2024-09-23 09:26:29 +0900

    스택

---
+++ src/Main_10828.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main_10828 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> stack = new ArrayList<>();
+        int N = sc.nextInt();
+
+        for (int i=0; i<N; i++) {
+            String command = sc.next();
+
+            switch (command) {
+                case "push":
+                    int value = sc.nextInt();
+                    stack.add(value);
+                    break;
+
+                case "pop":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    }
+                    else {
+                        System.out.println(stack.remove(stack.size() -1));
+                    }
+                    break;
+
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+
+                case "empty":
+                    System.out.println(stack.isEmpty() ? 1 : 0);
+                    break;
+
+                case "top":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    }
+                    else {
+                        System.out.println(stack.get(stack.size() -1));
+                    }
+                    break;
+            }
+        }
+        sc.close();
+    }
+}
