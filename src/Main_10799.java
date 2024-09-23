7e6d90596a923e98e89afa9a7ed8bc3634dd5a4d
tjdgusdk <sha6639@naver.com>
2024-09-23 11:31:09 +0900

    쇠막대기

---
+++ src/Main_10799.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main_10799 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine(); // 괄호 입력
+
+        Stack<Character> stack = new Stack<>();
+        int totalPieces = 0; // 총 잘린 조각 수
+
+        for (int i=0; i<input.length(); i++) {
+            char current = input.charAt(i);
+
+            if (current == '(') {
+                stack.push(current);
+            } else {
+                stack.pop();
+
+                if (input.charAt(i-1) == '(') {
+                    totalPieces += stack.size();
+                } else {
+                    totalPieces++;
+                }
+            }
+        }
+
+        System.out.println(totalPieces);
+        sc.close();
+    }
+}
