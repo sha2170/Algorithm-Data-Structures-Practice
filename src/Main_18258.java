43dc4cff602156c8594a5f64b8df6e305320b37f
tjdgusdk <sha6639@naver.com>
2024-09-27 14:52:59 +0900

    큐 2

---
+++ src/Main_18258.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+public class Main_18258 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        String command;
+        int last = -1;
+
+        for (int i=0; i<N; i++) {
+            command = br.readLine();
+
+            if (command.startsWith("push")) {
+                int X = Integer.parseInt(command.split(" ")[1]);
+                queue.offer(X);
+                last = X;
+            } else if (command.equals("pop")) {
+                if (queue.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(queue.poll() + "\n");
+                }
+            } else if (command.equals("size")) {
+                bw.write(queue.size() + "\n");
+            } else if (command.equals("empty")) {
+                bw.write((queue.isEmpty() ? 1 : 0) + "\n");
+            } else if (command.equals("front")) {
+                if (queue.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(queue.peek() + "\n");
+                }
+            } else if (command.equals("back")) {
+                if (queue.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(last + "\n");
+                }
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
