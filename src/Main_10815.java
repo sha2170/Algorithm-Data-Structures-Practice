5d92cde9ba5826aa2f41b83bcd779029555aed58
tjdgusdk <sha6639@naver.com>
2024-10-01 11:02:56 +0900

    숫자 카드

---
+++ src/Main_10815.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_10815 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int [] Cards = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            Cards[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(Cards);
+
+        int M = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+
+            if (binarySearch(Cards, target)) {
+                sb.append(1).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+
+    private static boolean binarySearch(int[] A, int target) {
+        int left = 0;
+        int right = A.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (A[mid] == target) {
+                return true;
+            } else if (A[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
