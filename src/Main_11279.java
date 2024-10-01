be9dedb5dfe1580f5a96fd7863ebe18fa9a96624
tjdgusdk <sha6639@naver.com>
2024-10-01 16:47:08 +0900

    최대 힙

---
+++ src/Main_11279.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.PriorityQueue;
+
+public class Main_11279 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+
+            if (x == 0) {
+                if (maxHeap.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(maxHeap.poll());
+                }
+            } else {
+                maxHeap.add(x);
+            }
+        }
+        sc.close();
+    }
+}
