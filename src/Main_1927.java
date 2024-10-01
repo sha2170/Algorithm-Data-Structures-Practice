19e0808d36e490334e01e32d07779aa74df2b660
tjdgusdk <sha6639@naver.com>
2024-10-01 16:42:09 +0900

    최소 힙

---
+++ src/Main_1927.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main_1927 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i=0; i<N; i++) {
+            int x = sc.nextInt();
+
+            if (x==0) {
+                if (minHeap.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(minHeap.poll());
+                }
+            } else {
+                minHeap.add(x);
+            }
+        }
+
+        sc.close();
+    }
+}
