6a1a3b22abf303c04ad60dfdf92b9a7737b6e9ac
tjdgusdk <sha6639@naver.com>
2024-10-01 20:16:42 +0900

    듣보잡

---
+++ src/Main_1764.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_1764 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 듣도 못한 사람 수
+        int M = Integer.parseInt(st.nextToken()); // 보도 못한 사람 수
+
+        HashSet<String> unheard = new HashSet<>();
+
+        for (int i=0; i<N; i++) {
+            unheard.add(br.readLine());
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i=0; i<M; i++) {
+            String name = br.readLine();
+            if (unheard.contains(name)) {
+                result.add(name);
+            }
+        }
+
+        Collections.sort(result);
+
+        bw.write(result.size() + "\n");
+
+        for (String name : result) {
+            bw.write(name + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
