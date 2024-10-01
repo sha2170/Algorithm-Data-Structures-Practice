1cc88ebc8396677a5c37172a0754a70ff1355ad4
tjdgusdk <sha6639@naver.com>
2024-10-01 16:28:31 +0900

    대칭 차집합

---
+++ src/Main_1269.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main_1269 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        HashSet<Integer> setA = new HashSet<>();
+        HashSet<Integer> setB = new HashSet<>();
+
+        for (int i=0; i<a; i++) {
+            setA.add(sc.nextInt());
+        }
+
+        for (int i=0; i<b; i++) {
+            setB.add(sc.nextInt());
+        }
+
+        HashSet<Integer> aMinusB = new HashSet<>(setA);
+        aMinusB.removeAll(setB);
+
+        HashSet<Integer> bminusA = new HashSet<>(setB);
+        bminusA.removeAll(setA);
+
+        int hab = aMinusB.size() + bminusA.size();
+
+        System.out.println(hab);
+        sc.close();
+    }
+}
