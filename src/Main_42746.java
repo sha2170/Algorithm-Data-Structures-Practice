cc2edb543f09800d38a29996cf40574d6470a361
tjdgusdk <sha6639@naver.com>
2024-10-29 13:47:29 +0900

    가장 큰 수

---
+++ src/Main_42746.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Main_42746 {
+    public String Main_42746(int[] numbers) {
+        String[] Numbers = new String[numbers.length];
+        for (int i=0; i<numbers.length; i++) {
+            Numbers[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(Numbers, (a, b) -> (b + a).compareTo(a + b));
+
+        if (Numbers[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String number : Numbers) {
+            result.append(number);
+        }
+
+        return result.toString();
+    }
+
+}
