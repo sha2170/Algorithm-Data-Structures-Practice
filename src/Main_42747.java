5b143de505d952a025ead0fdcaf5a102e59a02c2
tjdgusdk <sha6639@naver.com>
2024-10-29 13:51:00 +0900

    H-Index

---
+++ src/Main_42747.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Main_42747 {
+    public int main_42747(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+}
