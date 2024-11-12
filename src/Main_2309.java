46f21bf441a0ba2fb17c59be389750e4b7c971d1
tjdgusdk <sha6639@naver.com>
2024-11-12 17:25:44 +0900

    일곱 난쟁이

---
+++ src/Main_2309.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main_2309 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] skswoddl = new int[9];
+        int totalSum = 0;
+
+        for (int i=0; i<9; i++) {
+            skswoddl[i] = sc.nextInt();
+            totalSum += skswoddl[i];
+        }
+
+        // 불필요한 두 명 찾기
+        outerLoop:
+            for (int i=0; i<8; i++) {
+                for (int j=i+1; j<9; j++) {
+                    if (totalSum - skswoddl[i] - skswoddl[j] == 100) {
+                        skswoddl[i] = -1;
+                        skswoddl[j] = -1;
+                        break outerLoop;
+                    }
+                }
+            }
+
+            Arrays.sort(skswoddl);
+            for (int i=2; i<9; i++) {
+                System.out.println(skswoddl[i]);
+            }
+
+            sc.close();
+    }
+}
